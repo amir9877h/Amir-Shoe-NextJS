91640a489671536165667a9266efed16981c9c89
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2025-01-21 16:06:42 +0330

    create product details page change loading to skeleton

--- src/app/(front)/loading.tsx
+++
@@ -1,8 +0,0 @@
-import CustomLoading from "@/components/CustomLoading";
-import React from "react";
-
-const loading = () => {
-  return <CustomLoading message={`Please Wait...`} />;
-};
-
-export default loading;
